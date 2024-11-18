8d5f532d42ed01ebfac99344b745f7c443575b39
Zac Conlin <zac.conlin@gmail.com>
2024-11-18 12:23:28 -0700

    Updated desktop execute command

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # Initializing the setup script
 echo "Initializing the setup script..."
-sudo apt update && sudo apt upgrade -y
+#sudo apt update && sudo apt upgrade -y
 
 #### SETUP FILE STRUCTURE ####
 echo "##############################################"
@@ -24,4 +24,11 @@ chmod +x $HOME/Applications/Ghidra/ghidraRun
 mkdir -p $HOME/.local/share/applications
 sed -i "s/{HOME}/$(echo $HOME | sed 's/\//\\\//g')/g" ./init/ghidra/Ghidra.desktop
 cp ./init/ghidra/Ghidra.desktop $HOME/.local/share/applications
+chmod +x $HOME/.local/share/applications/Ghidra.desktop
+cp ./init/ghidra/Ghidra.desktop $HOME/Desktop
+chmod +x $HOME/Desktop/Ghidra.desktop
 cp ./init/ghidra/ghidra_icon_white.png $HOME/Applications/Ghidra
+sudo apt install openjdk-24-jdk -y
+
+# End of the setup script
+echo "Setup script complete!"
